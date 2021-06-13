a3b6f696ee29c8ae475fd75aac56f9f93281c9b5
dltech <dltech174@gmail.com>
2021-06-14 01:20:40 +0700

    hid desc

--- lib/joystick_desc.c
+++ lib/joystick_desc.c
@@ -1,101 +1,46 @@
 
-// desc ex from freejoy project
-uint8_t JoystickHID_ReportDescriptor[JoystickHID_SIZ_REPORT_DESC] =
-  {
-    0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
-    0x09, 0x04,                    // USAGE (Joystick)
-    0xa1, 0x01,                    // COLLECTION (Application)
 
-		0x85, REPORT_ID_JOY,				 	 //		REPORT_ID	(JOY_REPORT_ID)
+#define gamepadHidDescSize  7
+uint8_t gamepadHidDesc[gamepadHidDescSize] =
+{
+    0x07,       // bLenght
+    0x00,       // bDescriptorType
+    0x00, 0x00, // bcdHID
+    0x33,       // country code SU
+    0x01,       // bNumDescriptors
+    0x00,       // bDescriptorType
+    0x00, 0x37  // wDescriptorLenght
+}
 
-		// buttons data
-		0x05, 0x09,                    //   USAGE_PAGE (Button)
-    0x19, 0x01,                    //   USAGE_MINIMUM (Button 1)
-    0x29, MAX_BUTTONS_NUM,         //   USAGE_MAXIMUM (Button MAX_BUTTONS_NUM)
-    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
-    0x25, 0x01,                    //   LOGICAL_MAXIMUM (1)
-    0x75, 0x01,                    //   REPORT_SIZE (1)
-    0x95, MAX_BUTTONS_NUM,         //   REPORT_COUNT (MAX_BUTTONS_NUM)
-    0x81, 0x00,                    //   INPUT (Data,Ary,Abs)
-
-		// axis data
-		0x05, 0x01,                    // 	USAGE_PAGE (Generic Desktop)
-		0x09, 0x30,                    //   USAGE (X)
-    0x09, 0x31,                    //   USAGE (Y)
-    0x09, 0x32,                    //   USAGE (Z)
-    0x09, 0x33,                    //   USAGE (Rx)
-    0x09, 0x34,                    //   USAGE (Ry)
-    0x09, 0x35,                    //   USAGE (Rz)
-		0x09, 0x36,                    //   USAGE (Slider)
-		0x09, 0x36,                    //  	USAGE (Slider)
-    0x16, 0x01, 0x80,              //  	LOGICAL_MINIMUM (-32767)
-    0x26, 0xFF, 0x7F,						   //   LOGICAL_MAXIMUM (32767)
-    0x75, 0x10,                    //   REPORT_SIZE (16)
-    0x95, MAX_AXIS_NUM,       		 //   REPORT_COUNT (MAX_AXIS_NUM)
-    0x81, 0x02,                    //   INPUT (Data,Var,Abs)
-
-		// POV data
-		0x09, 0x39, 									 //   USAGE (Hat switch)
-		0x15, 0x00, 									 //   LOGICAL_MINIMUM (0)
-		0x25, 0x07, 									 //   LOGICAL_MAXIMUM (7)
-		0x35, 0x00, 									 //   PHYSICAL_MINIMUM (0)
-		0x46, 0x3B, 0x01,							 //   PHYSICAL_MAXIMUM (315)
-		0x65, 0x12, 									 //   UNIT (SI Rot:Angular Pos)
-		0x75, 0x08, 									 //   REPORT_SIZE (8)
-		0x95, 0x01, 								   //   REPORT_COUNT (1)
-		0x81, 0x02, 									 //   INPUT (Data,Var,Abs)
-		0x09, 0x39, 									 //   USAGE (Hat switch)
-		0x81, 0x02, 									 //   INPUT (Data,Var,Abs)
-		0x09, 0x39, 									 //   USAGE (Hat switch)
-		0x81, 0x02, 									 //   INPUT (Data,Var,Abs)
-		0x09, 0x39, 									 //   USAGE (Hat switch)
-		0x81, 0x02, 									 //   INPUT (Data,Var,Abs)
-
-		0xc0,                           // END_COLLECTION
-  }; /* CustomHID_ReportDescriptor */
-
-
-
-
-Usage Page (Generic Desktop),
-Usage (Joystick),
-Collection (Application),
-Usage Page (Generic Desktop),
-Usage (Pointer),
-Collection (Physical),
-Logical Minimum (-127),
-Logical Maximum (127),
-Report Size (8),
-Report Count (2),
-Push,
-Usage (X),
-Usage (Y),
-Input (Data, Variable, Absolute),
-Usage (Hat switch),
-Logical Minimum (0),
-Logical Maximum (3),
-Physical Minimum 0),
-Physical Maximum (270),
-Unit (Degrees),
-Report Count (1),
-Report Size (4),
-Input (Data, Variable, Absolute, Null State),
-Logical Minimum (0),
-Logical Maximum (1),
-Report Count (2),
-Report Size (1),
-Usage Page (Buttons),
-Usage Minimum (Button 1),
-Usage Maximum (Button 2),
-Unit (None),
-Input (Data, Variable, Absolute)
-End Collection,
-
-Usage Minimum (Button 3),
-Usage Minimum (Button 4),
-Input (Data, Variable, Absolute),
-Pop,
-Usage (Throttle),
-Report Count (1),
-Input (Data, Variable, Absolute),
-End Collection
+// report descriptor for gamepad w d-pad and two buttons
+#define gamepadreportDescSize   55
+uint8_t gamepadReportDesc[gamepadreportDescSize] =
+{
+    0x05, 0x01,         // usage_page(Generic Desctop)
+    0x09, 0x05,         // usage(Game pad)
+    0xa1, 0x01,         // collection(Application)
+    0x05, 0x01,         //  usage_page(Generic Desctop)
+    0x09, 0x01,         //  usage(Pointer)
+    0xa1, 0x00,         //   collection(Physical)
+    0x09, 0x39,         //   usage(Hat switch)
+    0x15, 0x00,         //   logical minimum(0)
+    0x25, 0x03,         //   logical maximum(3)
+    0x35, 0x00,         //   physical minimum(0)
+    0x46, 0x0e, 0x01    //   physical maximum(270)
+    0x65, 0x12,         //   unit(Sl Rot:Angular Pos)
+    0x95, 0x01,         //   report_count(1)
+    0x75, 0x04,         //   report_size(4)
+    0x81, 0x42,         //   input(Data,Var,Abs,Null)
+    0xc0                //  end_collection
+    0x05, 0x09,         //  usage_page(Button)
+    0x15, 0x00,         //  logical minimum(0)
+    0x25, 0x01,         //  logical maximum(1)
+    0x95, 0x02,         //  report_count(2)
+    0x75, 0x01,         //  report_size(1)
+    0x81, 0x02,         //  input(Data,Var,Abs)
+// byte alingment
+    0x95, 0x01,         //  report_count(1)
+    0x75, 0x02,         //  report_size(2)
+    0x81, 0x01,         //  input(Const)
+    0xc0                // end_collection
+};
